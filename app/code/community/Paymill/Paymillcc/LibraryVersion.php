0ffea4d39bb1f5e1be19055fc908b20089082572
Philipp Frank <philipp.alexander.frank@gmail.com>
2012-10-24 13:23:47 +0200

    Removed Version 2 Option

--- app/code/community/Paymill/Paymillcc/LibraryVersion.php
+++ app/code/community/Paymill/Paymillcc/LibraryVersion.php
@@ -4,8 +4,8 @@ class Paymill_Paymillcc_LibraryVersion
     public function toOptionArray()
     {
         return array(
-            array('value' => 'v1', 'label' => 'V1'),
-            array('value' => 'v2', 'label' => 'V2')
+            array('value' => 'v1', 'label' => 'V1')
+            //array('value' => 'v2', 'label' => 'V2')
         );
     }
 }
