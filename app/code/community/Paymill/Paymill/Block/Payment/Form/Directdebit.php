d583fa89c748caba4584f346c2a2d1faa618116e
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-20 10:45:16 +0200

    Made elv option beeing displayed during checkout. Issue #PM-310 - Migrierung des generischen Zahlungsformulars in das Plugin (elv)

--- app/code/community/Paymill/Paymill/Block/Payment/Form/Directdebit.php
+++ app/code/community/Paymill/Paymill/Block/Payment/Form/Directdebit.php
@@ -1,5 +1,14 @@
 <?php
 class Paymill_Paymill_Block_Payment_Form_Directdebit extends Paymill_Paymill_Block_Payment_Form_Abstract
 {
-    
+    /**
+     * Construct
+     */
+    protected function _construct()
+    {
+        Mage::log("ELV Form block");
+        parent::_construct();
+        $this->setTemplate('paymill/payment/form/directdebit.phtml');
+        
+    }
 }
--- app/code/community/Paymill/Paymill/Model/Method/Directdebit.php
+++ app/code/community/Paymill/Paymill/Model/Method/Directdebit.php
@@ -3,7 +3,16 @@ class Paymill_Paymill_Model_Method_Directdebit extends Paymill_Paymill_Model_Met
 {
     protected $_code = "paymill_directdebit";
     
-     /**
+    protected $_canSaveCc     = false;
+    
+    /**
+     * Form block identifier
+     * 
+     * @var string 
+     */
+    protected $_formBlockType = 'paymill/payment_form_directdebit';
+       
+    /**
      * Deals with payment processing when debit mode is active
      * @todo fill stub
      */
