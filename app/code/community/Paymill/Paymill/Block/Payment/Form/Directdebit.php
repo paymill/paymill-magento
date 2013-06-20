736c8bf3264400d8bf9ecf1a2815fb4178ba0ff4
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-20 12:19:23 +0200

    Removed Logging from direct debit form class

--- app/code/community/Paymill/Paymill/Block/Payment/Form/Directdebit.php
+++ app/code/community/Paymill/Paymill/Block/Payment/Form/Directdebit.php
@@ -6,7 +6,6 @@ class Paymill_Paymill_Block_Payment_Form_Directdebit extends Paymill_Paymill_Blo
      */
     protected function _construct()
     {
-        Mage::log("ELV Form block");
         parent::_construct();
         $this->setTemplate('paymill/payment/form/directdebit.phtml');
         
