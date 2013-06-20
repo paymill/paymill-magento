fa19451636650f78319e0262de9fbc83af5866df
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-20 09:49:32 +0200

    Moved assignData() to the abstract payment method class

--- app/code/community/Paymill/Paymill/Model/Method/Creditcard.php
+++ app/code/community/Paymill/Paymill/Model/Method/Creditcard.php
@@ -19,42 +19,16 @@ class Paymill_Paymill_Model_Method_Creditcard extends Paymill_Paymill_Model_Meth
     protected $_formBlockType = 'paymill/payment_form_creditcard';
        
     /**
-     * Assing data to information model object for fast checkout
-     * Saves Session Variables.
-     * @param mixed $data
-     */
-    public function assignData($data)
-    {
-        //Recieve Data
-        $postData = Mage::app()->getRequest()->getPost();
-        $token = $postData['payment']['paymill-payment-token'];
-        $tokenAmount = $postData['payment']['paymill-payment-amount'];
-        
-        //Save Data into session
-        Mage::getSingleton('core/session')->setToken($token);
-        Mage::getSingleton('core/session')->setTokenAmount($tokenAmount);
-        
-        //Save Data for FC
-        
-        
-        //Finish as usual
-        return parent::assignData($data);
-    }
-    
-    /**
      * Deals with payment processing when debit mode is active
      */
     public function debit()
     {
-        try{
-            $token = Mage::getSingleton('core/session')->getToken(); 
-            $tokenAmount = Mage::getSingleton('core/session')->getTokenAmount();
-            $paymentHelper = Mage::helper("paymill/payment");
-            $paymentProcessor = $paymentHelper->createPaymentProcessor($this->getCode(), $token, $tokenAmount);
-            $paymentProcessor->processPayment();
-        } catch (Exception $ex){
-            Mage::throwException($ex->getMessage());
-        }
+        $token = Mage::getSingleton('core/session')->getToken(); 
+        $tokenAmount = Mage::getSingleton('core/session')->getTokenAmount();
+        $paymentHelper = Mage::helper("paymill/payment");
+        $paymentProcessor = $paymentHelper->createPaymentProcessor($this->getCode(), $token, $tokenAmount);
+        $paymentProcessor->processPayment();
+        
     }
     
     /**
