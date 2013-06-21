9a3b2780a671b34c56306b62b9e899406516e477
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-21 13:57:36 +0200

    PM-317 Filled Remaining stubs in the FastCheckoutHelper

--- app/code/community/Paymill/Paymill/Helper/FastCheckoutHelper.php
+++ app/code/community/Paymill/Paymill/Helper/FastCheckoutHelper.php
@@ -13,34 +13,37 @@ class Paymill_Paymill_Helper_FastCheckoutHelper extends Mage_Core_Helper_Abstrac
         return Mage::helper("paymill/optionHelper")->isFastCheckoutEnabled();
     }
     
-    
     /**
      * Returns the clientId matched with the current user
      * @return String clientId
-     * @todo fill stub
      */
     public function getClientId()
     {
-        return null;
+        $userId = Mage::helper("paymill/customer")->getUserId();
+        return Mage::getModel("paymill/fastcheckout")->getClientId($userId);
     }
     
     /**
      * Returns the PaymentId associated with the current user
      * @param String $code PaymentMethodCode
      * @return String paymentId
-     * @todo fill stub
      */
     public function getPaymentId($code)
     {
-        return null;
+        $userId = Mage::helper("paymill/customer")->getUserId();
+        return Mage::getModel("paymill/fastcheckout")->getPaymentId($userId, $code);
     }
     
     /**
      * Returns a boolean describing whether there is saved fc data for the current user
+     * @param String $code PaymentMethodCode
      * @return boolean
-     * @todo fill stub
      */
-    public function hasData(){
+    public function hasData($code){
+        $userId = Mage::helper("paymill/customer")->getUserId();
+        if(Mage::getModel("paymill/fastcheckout")->hasFcData($userId, $code)){
+            return true;
+        }
         return false;
     }
     
