67641c0cff705238c12189d81b88cbd63ec39b5f
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-25 10:48:26 +0200

    PM-331 Fixing a bug causing the orderstate not to change on invoice creation

--- app/code/community/Paymill/Paymill/Model/Observer.php
+++ app/code/community/Paymill/Paymill/Model/Observer.php
@@ -24,9 +24,8 @@ class Paymill_Paymill_Model_Observer{
                 //Create the Invoice
                 Mage::helper('paymill/loggingHelper')->log(Mage::helper('paymill')->__($paymentCode), Mage::helper('paymill')->__('paymill_checkout_generating_invoice'), $orderId); 
                 $invoiceId = Mage::getModel('sales/order_invoice_api')->create($order->getIncrementId(), array());
-                $invoice = Mage::getModel('sales/order_invoice')->loadByIncrementId($invoiceId);
-                $invoice->capture()->save();
-                $order->setStatus(Mage_Sales_Model_Order::STATE_PROCESSING, false)->save();
+                Mage::getModel('sales/order_invoice_api')->capture($invoiceId);
+//                $order->setStatus(Mage_Sales_Model_Order::STATE_PROCESSING, false)->save();
             }
         }
     }
