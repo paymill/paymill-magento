690a24becbe26172b1e66e768af7342dab7c2b4c
Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-06-27 21:30:26 +0200

    PM-329 implemented delete action for the log table in the admin view

--- app/code/community/Paymill/Paymill/controllers/Adminhtml/LogController.php
+++ app/code/community/Paymill/Paymill/controllers/Adminhtml/LogController.php
@@ -22,4 +22,27 @@ class Paymill_Paymill_Adminhtml_LogController extends Mage_Adminhtml_Controller_
         $this->_initAction()
             ->renderLayout();
     }   
+    
+    /**
+     * Normal Magento delete mass action for selected entries
+     */
+    public function massDeleteAction()
+    {
+        $logIds = $this->getRequest()->getParam('log_id');
+
+        if (!is_array($logIds)) {
+            Mage::getSingleton('adminhtml/session')->addError(Mage::helper('paymill')->__('paymill_error_text_no_entry_selected'));
+        } else {
+            try {
+                foreach ($logIds as $logId) {
+                    Mage::getModel('paymill/log')->load($logId)->delete();
+                }
+                
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('paymill')->__("paymill_log_action_success"));
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+        $this->_redirect('*/*/index');
+    }
 }
