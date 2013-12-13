314d4d049cb6ce103ae194703d4d2c8f7ac66d33
kazemek <>
2013-12-13 16:05:22 +0100

    PMMA-27 add elv sepa

--- app/code/community/Paymill/Paymill/Block/Payment/Form/PaymentFormDirectdebit.php
+++ app/code/community/Paymill/Paymill/Block/Payment/Form/PaymentFormDirectdebit.php
@@ -30,25 +30,6 @@ class Paymill_Paymill_Block_Payment_Form_PaymentFormDirectdebit extends Paymill_
         $this->setTemplate('paymill/payment/form/directdebit.phtml');
     }
     
-    public function getPaymentData($code)
-    {
-        $payment = parent::getPaymentData($code);
-        
-        $data = array();
-        if (!is_null($payment)) {
-            $data['holder'] = $payment['holder'];
-            if (array_key_exists('code', $payment) && array_key_exists('account', $payment)) {
-                $data['code'] = $payment['code'];
-                $data['account'] = $payment['account'];
-            } elseif (array_key_exists('iban', $payment) && array_key_exists('bic', $payment)) {
-                $data['bic'] = $payment['bic'];
-                $data['iban'] = $payment['iban'];
-            }
-        }
-        
-        return $data;
-    }
-    
     public function isSepa()
     {
         return Mage::getStoreConfig('payment/paymill_directdebit/sepa', Mage::app()->getStore()->getStoreId()) ? 'true' : 'false';
