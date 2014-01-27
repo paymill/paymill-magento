b3d0714c3b507c52e2c411e06ec38df6acd724dc
kazemek <>
2014-01-27 14:37:40 +0100

    PMMA-37 add new credit card detection and some more supported brands

--- js/paymill/paymentForm.js
+++ js/paymill/paymentForm.js
@@ -47,12 +47,56 @@ function debug(message)
 	}
 }
 
+function detectCreditcardBranding(creditcardNumber) 
+ {
+	var brand = 'unknown';
+	if (creditcardNumber.match(/^\d{6}/)) {
+		switch (true) {
+			case /^(415006|497|407497|513)/.test(creditcardNumber):
+					brand = "carte-bleue";
+					break;
+			case /^(45399[78]|432913|5255)/.test(creditcardNumber):
+					brand = "carta-si";
+					break;
+			case /^(4571|5019)/.test(creditcardNumber):
+					brand = "dankort";
+					break;
+			case /^(62|88)/.test(creditcardNumber):
+					brand = "unionpay";
+					break;
+			case /^6(011|5)/.test(creditcardNumber):
+					brand = "discover";
+					break;
+			case /^3(0[0-5]|[68])/.test(creditcardNumber):
+					brand = "diners";
+					break;
+			case /^(5018|5020|5038|5893|6304|6759|6761|6762|6763|0604|6390)/.test(creditcardNumber):
+					brand = "maestro";
+					break;
+			case /^(2131|1800|35)/.test(creditcardNumber):
+					brand = "jcb";
+					break;
+			case /^(3[47])/.test(creditcardNumber):
+					brand = "amex";
+					break;
+			case /^(5[1-5])/.test(creditcardNumber):
+					brand = "mastercard";
+					break;
+			case /^(4)/.test(creditcardNumber):
+					brand = "visa";
+					break;
+		}
+	}
+
+	return brand;
+ }
+
 /**
  * Event Handler for the display of the card icons
  */
 function paymillShowCardIcon()
 {
-	var brand = paymill.cardType(pmQuery('#paymill_creditcard_number').val());
+	var brand = detectCreditcardBranding(pmQuery('#paymill_creditcard_number').val());
 	brand = brand.toLowerCase();
 	pmQuery("#paymill_creditcard_number")[0].className = pmQuery("#paymill_creditcard_number")[0].className.replace(/paymill-card-number-.*/g, '');
 	if (brand !== 'unknown') {
