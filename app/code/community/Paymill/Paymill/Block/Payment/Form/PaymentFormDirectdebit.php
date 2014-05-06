a19d451500b5697146e1a00cea56b611eb7ab5d5
Edip Fazli <edip.fazli@payintelligent.de>
2014-05-06 14:28:51 +0200

    PMMA-58 fixed fastcheckout for elv/sepa

--- app/code/community/Paymill/Paymill/Block/Payment/Form/PaymentFormDirectdebit.php
+++ app/code/community/Paymill/Paymill/Block/Payment/Form/PaymentFormDirectdebit.php
@@ -29,4 +29,18 @@ class Paymill_Paymill_Block_Payment_Form_PaymentFormDirectdebit extends Paymill_
         parent::_construct();
         $this->setTemplate('paymill/payment/form/directdebit.phtml');
     }
+
+    public function getPaymentEntryElv($code)
+    {
+        $data = $this->getPaymentData($code);
+        $fastCheckoutData = array(null,null);
+        if(isset($data['iban'])) {
+            $fastCheckoutData[0] = $data['iban'];
+            $fastCheckoutData[1] = $data['bic'];
+        } elseif(isset($data['account'])) {
+            $fastCheckoutData[0] = $data['account'];
+            $fastCheckoutData[1] = $data['code'];
+        }
+        return $fastCheckoutData;
+    }
 }
